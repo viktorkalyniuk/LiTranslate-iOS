ab3444a44d33fe98d05db735380d0c9aa26d05d4
Viktor Kalyniuk <@>
2022-08-12 17:38:51 +0300

    Initial Commit

---
+++ LibreTranslate/LibreTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibreTranslateApp.swift
+//  LibreTranslate
+//
+//  Created by vktr on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct LibreTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
